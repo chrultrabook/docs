5cdc5c816ac3c507dd47389fb7e6eb1980b86968
Ethan O'Brien <77750390+ethanaobrien@users.noreply.github.com>
2023-11-16 22:57:20 -0600

    More reliable fetching of device list

--- src/.vuepress/components/AddScript.vue
+++ src/.vuepress/components/AddScript.vue
@@ -16,20 +16,12 @@ export default {
   },
   methods: {
     loadExternalScript() {
-      (async() => {
-        if (!window.document) {
-          await new Promise(res => window.addEventListener("load", res));
-        }
-        if (document.readyState !== "loaded") {
-          await new Promise(res => document.addEventListener("load", res));
-        }
-        const script = document.createElement('script');
-        script.src = this.scriptUrl;
-        if (this.scriptId) {
-          script.id = this.scriptId;
-        }
-        document.body.appendChild(script);
-      })();
+      const script = document.createElement('script');
+      script.src = this.scriptUrl;
+      if (this.scriptId) {
+        script.id = this.scriptId;
+      }
+      document.body.appendChild(script);
     }
   }
 };
